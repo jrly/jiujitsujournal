d261883caedc1c2c54b362ff164088541eb8f76b
Jimmy Ly <jimmy4ly@gmail.com>
2025-05-01 13:30:10 -0700

    Initial Commit

---
+++ jiujitsujournal/jiujitsujournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  jiujitsujournalApp.swift
+//  jiujitsujournal
+//
+//  Created by Jimmy Ly on 2025-05-01.
+//
+
+import SwiftUI
+
+@main
+struct jiujitsujournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ jiujitsujournalTests/jiujitsujournalTests.swift
@@ -0,0 +1,17 @@
+//
+//  jiujitsujournalTests.swift
+//  jiujitsujournalTests
+//
+//  Created by Jimmy Ly on 2025-05-01.
+//
+
+import Testing
+@testable import jiujitsujournal
+
+struct jiujitsujournalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
